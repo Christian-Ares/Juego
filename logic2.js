1042101cd54d87201366f3a9e7eb5159d30ea91f
Christian Ares <christianaresvillan@gmail.com>
2020-11-04 21:08:26 +0100

    Images changed

--- logic2.js
+++ logic2.js
@@ -75,13 +75,13 @@ const createEnemies = () => {
 
 //Imagenes
 const enemySprite = new Image();
-enemySprite.src = "./Sprites/TMC_Octorok_Sprite.png";
+enemySprite.src = "/Sprites/TMC_Octorok_Sprite.png";
 const playerSprite = new Image();
-playerSprite.src = "./Sprites/images.png";
+playerSprite.src = "/Sprites/images.png";
 const background = new Image();
-background.src = "./Sprites/Fondo.png";
+background.src = "/Sprites/Fondo.png";
 const attackImage = new Image();
-attackImage.src = "./Sprites/TMC_Link_Sprite_7.png";
+attackImage.src = "/Sprites/TMC_Link_Sprite_7.png";
 
 
 //Constantes para dibujar
@@ -183,6 +183,7 @@ setInterval(() => {
 }, 2000);
 
 // Colisiones y Ataque
+
 const collision = () => {
     enemies.forEach((enemy, i) => {
         if (Math.abs(player.x - enemy.x) <= 30) {
